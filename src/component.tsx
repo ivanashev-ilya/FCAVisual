a2c68c99683edc8a1e5ef5c2f1ae7a5216da6332
ivanashev ilja <ivanashevi@gmail.com>
2021-12-12 11:42:30 +0300

    Added ideals and filters

--- src/component.tsx
+++ src/component.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 
 export class Point {
     constructor(X: number, Y: number) {
@@ -49,36 +50,122 @@ export class ReactConceptLattice extends React.Component<{}, State>{
     constructor(props: any){
         super(props);
         this.state = initialState;
+        this.addFilter = this.addFilter.bind(this);
+        this.addIdeal = this.addIdeal.bind(this);
+    }
+
+    addFilter(id: number) {
+        this.filterConcept = id;
+        this.idealConcept = -1;
+        ReactConceptLattice.update(this.state);
+    }
+
+    addIdeal(id: number) {
+        this.idealConcept = id;
+        this.filterConcept = -1;
+        ReactConceptLattice.update(this.state);
+    }
+
+    removeFilterAndIdeal() {
+        this.idealConcept = -1;
+        this.filterConcept = -1;
+        ReactConceptLattice.update(this.state);
+    }
+
+    dfs(cur: number, adjacencyLists: number[][], nodes: Set<number>) {
+        nodes.add(cur);
+        for (var neigh of adjacencyLists[cur]) {
+            this.dfs(neigh, adjacencyLists, nodes);
+        }
+    }
+
+    getFilterOrIdealNodes(start: number, conceptsCount: number, edges: Edge[],
+        nodes: Set<number>, type: string)
+    {
+        var adjacencyLists = [];
+        for (var i = 0; i < conceptsCount; ++i) {
+            adjacencyLists[i] = [];
+        }
+        for (var edge of edges) {
+            if (type == "ideal") {
+                adjacencyLists[edge.beg].push(edge.end);
+            } else {
+                adjacencyLists[edge.end].push(edge.beg);
+            }
+        }
+        console.error(adjacencyLists);
+        this.dfs(start, adjacencyLists, nodes);
     }
 
     render(){
         const { width, height, nodeWidth, nodeHeight, coords,
             conceptsAttributes, conceptsObjects, edges, background, lineThickness } = this.state;
 
-        var shapes = [<svg width={width} height={height}>{
-            edges.map((edge) => {
-                return <line x1={coords[edge.beg].x} y1={coords[edge.beg].y}
-                    x2={coords[edge.end].x} y2={coords[edge.end].y}
-                    stroke="black" strokeWidth={lineThickness}/>;
-            })
-        }</svg>];
+        var shapes = [
+            <div>
+                <ContextMenuTrigger id={"Background"}>
+                    <svg width={width} height={height}>{
+                        edges.map((edge) => {
+                            return <line x1={coords[edge.beg].x} y1={coords[edge.beg].y}
+                                x2={coords[edge.end].x} y2={coords[edge.end].y}
+                                stroke="black" strokeWidth={lineThickness}/>;
+                        })
+                    }</svg>
+                </ContextMenuTrigger>
+            
+                <ContextMenu id={"Background"}>
+                    <MenuItem onClick={() => this.removeFilterAndIdeal()}>
+                    Hide filter/ideal
+                    </MenuItem>
+                </ContextMenu>
+            </div>
+        ];
 
-        for (var i = 0; i < coords.length; ++i) {
+        var filterNodes = new Set<number>();
+        var idealNodes = new Set<number>();
+        if (this.filterConcept != -1) {
+            this.getFilterOrIdealNodes(this.filterConcept, coords.length, edges, filterNodes, "filter");
+        } else if (this.idealConcept != -1) {
+            this.getFilterOrIdealNodes(this.idealConcept, coords.length, edges, idealNodes, "ideal");
+        }
+
+        for (let i = 0; i < coords.length; ++i) {
+            var borderColor = "black";
+            if (filterNodes.has(i)) {
+                borderColor = "green";
+            } else if (idealNodes.has(i)) {
+                borderColor = "red";
+            }
             const style: React.CSSProperties = { 
                 width: nodeWidth, height: nodeHeight,
                 left: coords[i].x - nodeWidth / 2,
                 top: coords[i].y - nodeHeight / 2,
                 position: "absolute",
                 background: background,
-                borderWidth: lineThickness
+                borderWidth: lineThickness,
+                borderColor: borderColor
             };
             shapes.push(
-                <div className="conceptNode" style={style}>
-                    <p>
-                        {conceptsObjects[i]}
-                        <br/>
-                        <em>{conceptsAttributes[i]}</em>
-                    </p>
+                <div>
+                    <ContextMenuTrigger id={String(i)}>
+                        <div className="conceptNode" style={style}>
+                            <p>
+                                {conceptsObjects[i]}
+                                <br/>
+                                <em>{conceptsAttributes[i]}</em>
+                            </p>
+                        </div>
+                    </ContextMenuTrigger>
+                
+                    <ContextMenu id={String(i)}>
+                        <MenuItem onClick={() => this.addFilter(i)}>
+                        Draw filter
+                        </MenuItem>
+                        <MenuItem divider />
+                        <MenuItem onClick={() => this.addIdeal(i)}>
+                        Draw ideal
+                        </MenuItem>
+                    </ContextMenu>
                 </div>
             )
         }
@@ -95,6 +182,8 @@ export class ReactConceptLattice extends React.Component<{}, State>{
     }
 
     public state: State = initialState;
+    public filterConcept: number = -1;
+    public idealConcept: number = -1;
 
     public componentWillMount() {
         ReactConceptLattice.updateCallback = (newState: State): void => { this.setState(newState); };
